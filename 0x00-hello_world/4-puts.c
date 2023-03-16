dd3ab60c6fb93942bb0c20425d296626632a2124
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-03-17 01:16:53 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-int main (void){puts("\"Programming is like building a multilingual puzzle);return (0);}
+#include <stdio.h>
+
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle);
+return (0);
+}
