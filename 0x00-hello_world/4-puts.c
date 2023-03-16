0b45e9bbad83cf477b6429f658e97bf1b135775d
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-03-17 01:20:23 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
 * main - this is the main function
 *
-* Return - always 0
+* Return: always 0
 */
 
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle);
 return (0);
