aef3c1c82fc573dfd227e658520a1352871263ce
AlaaEldinTarek <3laa.eldin.tarek@gmail.com>
2023-03-17 16:21:30 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+char alp[26] = abcdefghijklmnopqrstuvwxyz;
 int i;
 
 for (i = 0; i < 26; i++)
 {
 putchar(alp[i]);
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
 
