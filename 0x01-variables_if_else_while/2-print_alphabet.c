fcd05648b25d40d332625b33b976513872268b21
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-03-18 01:27:20 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-char alp[26] = abcdefghijklmnopqrstuvwxyz;
-int i;
-
-for (i = 0; i < 26; i++)
-{
-putchar(alp[i]);
-}
+char i;
+  for (i='a'; i<='z'; i++)
+    putchar(i);
+  for (i='A'; i<='Z'; i++)
+    putchar(i);
 putchar('\n');
 return (0);
 }
