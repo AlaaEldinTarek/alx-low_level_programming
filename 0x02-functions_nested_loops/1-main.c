17fde173f8d8e70cac26dfa46176f1bce5aa74b3
AlaaEldinTarek <3laa.eldin.tarek@gmail.com>
2023-03-22 19:51:43 -0700

    1-main.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,13 +2,14 @@
 
 
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet();
+    print_alphabet_x10();
     return (0);
 }
+
 
