e13eb1f9c3b1693f97080d482835881d174f7895
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-03-24 02:47:56 +0200

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,11 +1,4 @@
 #include "main.h"
-/**
- * positive_or_negative - checks for positive or negative numbers
- *
- * @i : the unumber to be checked
- *
- * Return : always 0
- */
 void positive_or_negative(int i) 
 {
   if (i < 0)
