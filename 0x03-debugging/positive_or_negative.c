e5d1f09dd24f524303945c210aa20ad22bb7d3a6
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-03-24 02:54:25 +0200

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,9 +2,9 @@
 void positive_or_negative(int i) 
 {
 if (i < 0)
-printf("%d is negative\n", i);
+ printf("%d is negative\n", i);
 else if (i > 0)
-printf("%d is positive\n", i);
+ printf("%d is positive\n", i);
 else
-printf("%d is zero\n", i);
+ printf("%d is zero\n", i);
 }
