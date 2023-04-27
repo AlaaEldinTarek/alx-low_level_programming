9fad74a99987e0abe52dcd6b33002e83d13c62eb
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-04-28 00:01:23 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,18 @@
+/*
+ * File: 100-first.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void __attribute__((constructor)) hare(void);
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-void first(void)
+void hare(void)
 {
- printf("You're beat! and yet, you must allow,\n");
- printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
-
