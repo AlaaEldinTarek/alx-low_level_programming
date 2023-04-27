a748f744eaaa7c480d000e6897f09b0faa4f6aeb
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-04-27 23:57:58 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,45 @@
-#include <stdlib.h>
-#include <string.h>
+/*
+ * File: 2-add_node.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
  *
- * Return: the address of the new element, or NULL if it fails
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *new;
- unsigned int len = 0;
+	char *dup;
+	int len;
+	list_t *new;
 
- while (str[len])
- len++;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
+	for (len = 0; str[len];)
+		len++;
 
- return (*head);
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
-
