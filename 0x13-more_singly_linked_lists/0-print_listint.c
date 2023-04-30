a3fe51c974895845a272f7bcd50f35a668292fbf
Alaa Eldin Tarek <xoxmadoxox76@gmail.com>
2023-04-30 08:26:32 -0700

    done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+/*
+ * file: 0-print_listint.c
+ * auth: Alaa Eldin
+ */
+
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all the elements of a listint_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: the number of nodes in the list_t list.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		prinf("%d\n", h->n);
+		h = h-next;
+	}
+	return (nodes);
+}
