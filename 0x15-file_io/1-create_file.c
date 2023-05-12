03ab113cd08afb9bfc9b5c252a394949c5875932
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-05-12 23:01:41 +0300

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,16 +6,16 @@
  *
  * Return: integer length of string
  */
-int _strlen(char *t)
+int _strlen(char *s)
 {
-	int r = 0;
+	int i = 0;
 
-	if (!t)
+	if (!s)
 		return (0);
 
-	while (*t++)
-		r++;
-	return (r);
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
@@ -27,16 +27,16 @@ int _strlen(char *t)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int q;
+	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	q = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (q == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 	if (len)
-		bytes = write(q, text_content, len);
-	close(q);
+		bytes = write(fd, text_content, len);
+	close(fd);
 	return (bytes == len ? 1 : -1);
 }
