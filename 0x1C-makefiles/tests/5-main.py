d874df267ad2a8afaa342e71b71bf58ba21ad06b
AlaaEldinTarek <3laa.eldin.tarek@gmail.com>
2023-08-23 00:20:35 -0700

    done

---
+++ 0x1C-makefiles/tests/5-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+5-main
+"""
+island_perimeter = __import__('5-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
+
+guillaume@ubuntu:~/0x1C$ 
+guillaume@ubuntu:~/0x1C$ ./5-main.py
+12
+guillaume@ubuntu:~/0x1C$ 
